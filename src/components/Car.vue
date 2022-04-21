61e15bade59de014c7c9a30b9612d0944b24cf0e
Alexandr <vampir666x@gmail.com>
2022-04-21 21:58:44 +0300

    Создал страницу для отображения информации об автомобиле

---
+++ src/components/Car.vue
@@ -0,0 +1,21 @@
+<template>
+<h1>МАШИИИИИНААААА {{number}}</h1>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'Car',
+  props: {
+    number: {
+      type: String,
+      default: '0'
+    }
+  }
+})
+</script>
+
+<style scoped>
+
+</style>
